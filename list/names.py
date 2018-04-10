73f393ca185f321e172d3bfbbbd7a4df0fe67be2
LyonDon <wangyidl258@163.com>
2018-04-10 22:31:34 +0800

    添加了list文件夹

---
+++ list/names.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+names=["Bob","Lucy","Kobe","Harden"]
+for i in range(4):
+	print(names[i])
+messsage="my friend"
+for j in range(4):
+	print(names[j]+" "+messsage)
