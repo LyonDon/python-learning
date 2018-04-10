05c46ee79961c4a33758d4aa14abf26fc84abc6d
Dong <wangyidl258@163.com>
2018-04-10 19:43:55 +0800

    添加simple_message.py文件

---
+++ simple_message.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+message="这是一条消息，等下需要打印出来"
+print(message)
+message="这是一条新消息"
+print(message)
+print(3+1+4)
+print(9-1)
+print(2*4)
+print(24/3)
+
+num=10
+print("my favorite num is " + str(num))
