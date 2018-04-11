8ba6ee0865a69914143cdc9409ef1e54cbdc6462
LyonDon <wangyidl258@163.com>
2018-04-11 22:51:44 +0800

    添加一个guest文件

---
+++ list/guest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+list=['Bill Gates','Steve Jobs','Kobe']
+print(list)
+print(list[0]+" can't come because he's too busy\nSo,I need to make a plan B")
+list[0]='Yao Ming'
+print(list)
+list.append('Jay lee')
+list.append("周星驰")
+list.append('Jackie Chan')
+print(str(list).decode('string_escape'))
+list.insert(0,'Trump')
+print(str(list).decode('string_escape'))
+
+print("sorry that I can only invite two people")
+poped=list.pop(0)
+print('sorry'+' '+poped)
+poped=list.pop(0)
+print('sorry'+' '+poped)
+poped=list.pop(0)
+print('sorry'+' '+poped)
+poped=list.pop(0)
+print('sorry'+' '+poped)
+poped=list.pop(0)
+print('sorry'+' '+poped)
+
+#print(str(list).decode('string_escape'))
+print(list[0]+' you are still invited')
+print(list[1]+' you are still invited')
+
+del list[0]
+del list[0]
+print(list)
