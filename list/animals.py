efddea2d739dee041e293b3699543d1ecdf4b53b
LyonDon <wangyidl258@163.com>
2018-04-17 10:47:25 +0800

    修改了animal文件

--- list/animals.py
+++ list/animals.py
@@ -1,6 +1,15 @@
+#coding:utf-8
 animals=['lion','tiger','cat']
 for animal in animals:
 	print(animal)
 	print('The '+animal+' is a feline.\n')
 	pass
-print('They have something in common')
+print('They have something in common\n')
+
+#其他的一些尝试
+squares=[]
+for value in range(1,11):
+	squares.append(value**2)	
+	pass
+#print('\n')
+print(squares)
