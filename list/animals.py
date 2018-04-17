c8013615719a6910ccb1a3ed9188518523fb7745
LyonDon <wangyidl258@163.com>
2018-04-17 10:21:19 +0800

    添加了animals和pizza文件

---
+++ list/animals.py
@@ -0,0 +1,6 @@
+animals=['lion','tiger','cat']
+for animal in animals:
+	print(animal)
+	print('The '+animal+' is a feline.\n')
+	pass
+print('They have something in common')
---
+++ list/pizza.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+pizzas=['KFC','Mac','PizzaHut']
+
+#打印pizzas列表中的pizza元素
+for pizza in pizzas:
+	print(pizza)
+	pass
+print('\n')
+
+#打印包含pizza名称的句子
+for pizza in pizzas:
+	print("I like "+pizza+" pizza")
+	pass
+
+print('\nI like pepperoni pizza')
