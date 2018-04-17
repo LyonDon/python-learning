6844a6c2b4cd7cd5958ddae4ea40ada53a596f50
LyonDon <wangyidl258@163.com>
2018-04-17 20:59:09 +0800

    添加了元祖文件，元祖与列表的区别在于元祖不可修改

---
+++ demension/food.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+foods=('fish','chicken','beef','orange','apple')
+for food in foods:
+	print(food)
+	pass
+print(' ')
+#foods[1]='meat'
+foods=('meat','milk','beef','orange','apple')
+for food in foods:
+	print(food)
+	pass
