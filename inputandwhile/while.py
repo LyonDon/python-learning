ae355e52d1b4eda25d268225610d88425cf451bb
LyonDon <wangyidl258@163.com>
2018-04-20 12:09:15 +0800

    修改了while文件,练习用户输入填充字典

--- inputandwhile/while.py
+++ inputandwhile/while.py
@@ -30,9 +30,46 @@ while True:
 
 #无限循环
 #编辑器崩了
+'''
 message=input("please input a number")
 num=int(message)
 
 while num<=20:
 	print(num)
 	pass
+'''
+
+#三明治列表
+sandwish_orders=['a','s','cs','pastrami','pastrami','pastrami']
+print('pastrami has been sold out')
+print(sandwish_orders)
+
+while 'pastrami' in sandwish_orders:
+		sandwish_orders.remove('pastrami')
+		pass
+finished_sandwiches=[]
+
+for sandwish in sandwish_orders:
+	print(sandwish+' I made your tuna sandwish')
+	finished_sandwiches.append(sandwish)
+	pass
+
+print(finished_sandwiches)
+
+#用户输入填充字典,若是使用列表则只需一个输入值
+holiday_palces={}
+
+while True:
+	question=input('\nwhere do you want to go:')
+	holiday_palce=input('\n')
+	holiday_palces[question]=holiday_palce
+
+	repeat=raw_input('\ndoes anyone want to have a test?')
+	if repeat=='No':
+		break
+		pass
+	else:
+		continue
+
+print('result----------')
+print(holiday_palces)
