5387044ad9ec4bba3eedb51b7ed2c4ca917fff37
LyonDon <wangyidl258@163.com>
2018-04-25 21:27:16 +0800

    学习函数模块中的函数调用

---
+++ hanshu/module_call.py
@@ -0,0 +1,7 @@
+from user_profile import build_profile
+from user_profile import *
+
+her=build_profile('Zhang','Ting',sex="female",age="26")
+print(her)
+make_car('BMW','big',color='yellow',cost='200000')
+
