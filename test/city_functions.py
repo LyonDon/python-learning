d562efe26e7164df9eb58ccf4af67cb938758c73
LyonDon <wangyidl258@163.com>
2018-04-28 19:12:24 +0800

    创建一系列文件，学习测试的用法

---
+++ test/city_functions.py
@@ -0,0 +1,4 @@
+def city_country(city,country,population=''):
+	string=city+","+country+"-"+population
+	return string.title()
+	pass
---
+++ test/test_citie_countty_population.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+import unittest
+from city_functions import city_country
+
+class CityTestsCase(unittest.TestCase):
+	"""测试city_functions.py"""
+	def test_city_country(self):
+		"""能否正确处理输入"""
+		example=city_country("beijing",'china','population=300000')
+		pass
+
+unittest.main()
